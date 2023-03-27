f6884ba04e6f6c98e50710760c75002b9f6346c0
GilmarMMJr <gilmar_mendonca@live.com>
2023-03-27 20:25:00 -0300

    finish Character Photo Cell

--- RickAndMorty/RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -96,7 +96,7 @@ extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectio
                 fatalError()
             }
             cell.configure(with: viewModel)
-            cell.backgroundColor = .systemYellow
+//            cell.backgroundColor = .systemYellow
             return cell
         case .information(let viewModels):
             guard let cell = collectionView.dequeueReusableCell(
@@ -106,7 +106,7 @@ extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectio
                 fatalError()
             }
             cell.configure(with: viewModels[indexPath.row])
-            cell.backgroundColor = .systemRed
+//            cell.backgroundColor = .systemRed
             return cell
         case .episodes(let viewModels):
             guard let cell = collectionView.dequeueReusableCell(
@@ -116,7 +116,7 @@ extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectio
                 fatalError()
             }
             cell.configure(with: viewModels[indexPath.row])
-            cell.backgroundColor = .systemOrange
+//            cell.backgroundColor = .systemOrange
             return cell
         }
     }
